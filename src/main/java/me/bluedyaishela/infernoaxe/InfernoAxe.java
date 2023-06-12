90eb5f2f577654db46a91bcb1e19d341612fff8b
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-06-12 15:40:57 +0200

    init project

---
+++ src/main/java/me/bluedyaishela/infernoaxe/InfernoAxe.java
@@ -0,0 +1,17 @@
+package me.bluedyaishela.infernoaxe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InfernoAxe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
